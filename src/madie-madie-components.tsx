c4800baafdf4aaf28309aff0cf6da0c94ea5b472
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2021-10-21 12:12:21 -0400

    Set up exports from Components app
    - Exporting Button and TextInput.
    - Shifting local port to 8504 to prevent port conflict with madie-public.

--- src/madie-madie-components.tsx
+++ src/madie-madie-components.tsx
@@ -1,2 +1,21 @@
-// Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
+import singleSpaReact from "single-spa-react";
+import Root from "./root.component";
+import { TextInputComponent } from "./components/inputs/TextInput";
+
+// exporting the default function requires the following syntax /shrug
+export { default as Button } from "./components/buttons/Button";
+
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: Root,
+  errorBoundary(err, info, props) {
+    // Customize the root error boundary for your microfrontend here.
+    return null;
+  },
+});
+
+export const TextInput = TextInputComponent;
+export const { bootstrap, mount, unmount } = lifecycles;
